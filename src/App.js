5ef1da79d5e2b01aee6e04524054a4af57e350d0
vi_mi <fenil.jain2018@vitstudent.ac.in>
2020-04-21 12:08:50 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Namastey Duniyaa!
         </a>
       </header>
     </div>
